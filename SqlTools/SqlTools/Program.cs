72ae71dd7d3d03edcde43798cdadfe968df1d882
Marc <marcs@autotaal.com>
2021-07-14 16:19:58 +0200

    first

---
+++ SqlTools/SqlTools/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SqlTools
+{
+  class Program
+  {
+    static void Main(string[] args) => new Program().Start(args);
+
+    public int Start(string[] args)
+    {
+      return 0;
+    }
+  }
+}
